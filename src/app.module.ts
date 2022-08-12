d7d3ecc76c0c4e37d5992838968fca80ffdacbff
Mustafa <mu9tafa.805@gmail.com>
2022-08-12 21:29:06 +0200

    Adding .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,18 @@ import { TaskEntity } from './modules/task/task.entity';
 import { UserEntity } from './modules/user/user.entity';
 import { StatusEntity } from './modules/status/status.entity';
 import { LoggerMiddleware } from './middlewares/logger-middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-      password: 'ghyhgfhghhggi',
-      database: 'nest-task-manager-api',
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_USER,
       synchronize: true,
       entities: [TaskEntity, UserEntity, StatusEntity],
     }),
